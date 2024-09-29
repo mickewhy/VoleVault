f9f0532f49a97b59d2af80c8ab974305e978624b
Mickey <74501941+mickewhy@users.noreply.github.com>
2024-09-29 21:33:31 +0300

    Fetches from approved images only

--- backend/controllers/suborderController.js
+++ backend/controllers/suborderController.js
@@ -3,37 +3,37 @@ const mongoose = require('mongoose')
 
 // Get all Anomaluromorpha spp.
 const getAnomaluromorpha = async (req, res) => {
-    const rodentQuery = await rodents.find({ suborder: "Anomaluromorpha" }).sort({ family: 1, binomialName: 1 })
+    const rodentQuery = await rodents.find({ suborder: "Anomaluromorpha", isApproved: true }).sort({ family: 1, binomialName: 1 })
     res.status(200).json(rodentQuery)
 }
 
 // Get all Castorimorpha spp.
 const getCastorimorpha = async (req, res) => {
-    const rodentQuery = await rodents.find({ suborder: "Castorimorpha" }).sort({ family: 1, binomialName: 1 })
+    const rodentQuery = await rodents.find({ suborder: "Castorimorpha", isApproved: true }).sort({ family: 1, binomialName: 1 })
     res.status(200).json(rodentQuery)
 }
 
 // Get all Hystricomorpha spp.
 const getHystricomorpha = async (req, res) => {
-    const rodentQuery = await rodents.find({ suborder: "Hystricomorpha" }).sort({ family: 1, binomialName: 1 })
+    const rodentQuery = await rodents.find({ suborder: "Hystricomorpha", isApproved: true }).sort({ family: 1, binomialName: 1 })
     res.status(200).json(rodentQuery)
 }
 
 // Get all Myomorpha spp.
 const getMyomorpha = async (req, res) => {
-    const rodentQuery = await rodents.find({ suborder: "Myomorpha" }).sort({ family: 1, binomialName: 1 })
+    const rodentQuery = await rodents.find({ suborder: "Myomorpha", isApproved: true }).sort({ family: 1, binomialName: 1 })
     res.status(200).json(rodentQuery)
 }
 
 // Get all Sciuromorpha spp.
 const getSciuromorpha = async (req, res) => {
-    const rodentQuery = await rodents.find({ suborder: "Sciuromorpha" }).sort({ family: 1, binomialName: 1 })
+    const rodentQuery = await rodents.find({ suborder: "Sciuromorpha", isApproved: true }).sort({ family: 1, binomialName: 1 })
     res.status(200).json(rodentQuery)
 }
 
 // Get all spp.
 const getRodents = async (req, res) => {
-    const rodentQuery = await rodents.find().sort({ family: 1, binomialName: 1 })
+    const rodentQuery = await rodents.find({ isApproved: true }).sort({ family: 1, binomialName: 1 })
     res.status(200).json(rodentQuery)
 }
 
