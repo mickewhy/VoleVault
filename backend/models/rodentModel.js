2c940a34542d00fc060ed5e4590e8c0282e7108d
Mickey <74501941+mickewhy@users.noreply.github.com>
2024-10-06 20:27:42 +0300

    Update rodentModel.js

--- backend/models/rodentModel.js
+++ backend/models/rodentModel.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose')
 
 const rodentSchema = new mongoose.Schema({
-    commonName: { type: String, required: false, unique: false },
-    binomialName: { type: String, required: false, unique: false },
-    suborder: { type: String, required: false, unique: false },
-    family: { type: String, required: false, unique: false },
+    commonName: { type: String, required: true, unique: false },
+    binomialName: { type: String, required: true, unique: false },
+    suborder: { type: String, required: true, unique: false },
+    family: { type: String, required: true, unique: false },
     sex: { type: String, required: false, unique: false },
     age: { type: String, required: false, unique: false },
     origin: { type: Array, required: false, unique: false },
-    dateOfAcquisition: { type: Date, required: false, unique: false },
+    dateOfAcquisition: { type: Date, required: true, unique: false },
     causeOfDeath: { type: String, required: false, unique: false },
-    dimensions: { type: Array, required: false, unique: false },
+    dimensions: { type: Array, required: true, unique: false },
     cleaningMethod: { type: String, required: false, unique: false },
     notes: { type: String, required: false, unique: false },
-    imageLinks: { type: Array, required: false, unique: false, validate: [arrayLimit, '{PATH} exceeds the limit of 6'] },
-    credit: { type: String, required: false, unique: false },
+    imageLinks: { type: Array, required: true, unique: false, validate: [arrayLimit, 'The image array exceeds the limit of 6'] },
+    credit: { type: String, required: true, unique: false },
     copyrightInfo: { type: String, required: false, unique: false },
-    isApproved: { type: Boolean, required: false, unique: false },
-    username: { type: String, required: false, unique: false },
+    isApproved: { type: Boolean, required: true, unique: false },
+    username: { type: String, required: true, unique: false },
 }, { timestamps: true })
 
 function arrayLimit(val) {
